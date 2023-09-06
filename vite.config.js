746fbc41844367b3dcf992f58b9c85b309ad87fc
natsm <natsmarinho@gmail.com>
2023-09-06 08:16:34 -0300

    feat: url do repositorio adicionado na base do vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/styles/",
+  base: "https://natsmarinho.github.io/style-store/",
   plugins: [],
   resolve: {
     /*something*/
