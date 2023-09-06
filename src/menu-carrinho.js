a164efc1b0a76a01188b7cf848ef5a56bc5cec09
natsm <natsmarinho@gmail.com>
2023-09-06 08:19:22 -0300

    fix: caminho para checkout corrigido

--- src/menu-carrinho.js
+++ src/menu-carrinho.js
@@ -16,7 +16,7 @@ function irParaCheckout() {
   if(Object.keys(idProdutoCarrinhoQuantidade).length === 0) {
     return;
   }
-  window.location.href = window.location.origin + "/checkout.html"
+  window.location.href = "./checkout.html"
 }
 
 export function inicializaCarrinho() {
