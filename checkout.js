aeb7e0d947dc7a4d7bb63d0761bcc80c8da7ddde
natsm <natsmarinho@gmail.com>
2024-02-20 08:26:42 -0300

    feat: adiciona espaco apos quatro numeros do cartao digitado

--- checkout.js
+++ checkout.js
@@ -1,5 +1,20 @@
 import { apagarDoLocalStorage, desenharProdutoNoCarrinhoSimples, lerLocalStorage, salvarLocalStorage } from "./src/utilidades";
 
+document.getElementById("numero-cartao").addEventListener("input", function (e) {
+    let inputCartaoCredito = e.target;
+    const regexCartao = inputCartaoCredito.value.replace(/\s+/g, '').replace(/[^0-9]/gi, '');
+    const formatoCartaoNaTela = "";
+
+    for (let i = 0; i < regexCartao.length; i++) {
+        if (i > 0 && i % 4 === 0) {
+            formatoCartaoNaTela += " ";
+        }
+        formatoCartaoNaTela += regexCartao[i];
+    }
+
+    inputCartaoCredito.value = formatoCartaoNaTela;
+})
+
 function desenharProdutosCheckout() {
     const idProdutoCarrinhoQuantidade = lerLocalStorage("carrinho") ?? {};
 
@@ -7,6 +22,7 @@ function desenharProdutosCheckout() {
         desenharProdutoNoCarrinhoSimples(idProduto, "container-produtos-checkout", idProdutoCarrinhoQuantidade[idProduto]);
     }
 }
+
 
 function finalizarCompra(evento) {
     evento.preventDefault();
